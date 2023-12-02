bb404b679e1472260bb83007111962543580e29c
devonfox <devonhouston.fox@gmail.com>
2023-12-01 21:33:10 -0800

    added file reader

--- day2/main.go
+++ day2/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
+type Roll struct {
+	red   int
+	blue  int
+	green int
+}
+
+func part1() int {
+	puzzleInput := "input.txt"
+
+	file, _ := os.Open(puzzleInput)
+
+	buffer := bufio.NewScanner(file)
+
+	sum := 0
+
+	for buffer.Scan() {
+		line := buffer.Text()
+		println(line)
+	}
+	return sum
+}
+
 func main() {
-	println("OK")
+	println("Part 1: ", part1())
 }
