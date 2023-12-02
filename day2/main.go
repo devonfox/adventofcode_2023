7392d6144b8c0a44adb35379c0fee7b3889d71e3
devonfox <devonhouston.fox@gmail.com>
2023-12-02 10:42:23 -0800

    day2 part2 complete

--- day2/main.go
+++ day2/main.go
@@ -84,7 +84,58 @@ func part1() int {
 
 func part2() int {
 	// Test input should be: 2286
-	return 0
+	puzzleInput := "input.txt"
+
+	file, _ := os.Open(puzzleInput)
+
+	buffer := bufio.NewScanner(file)
+
+	sum := 0
+
+	for buffer.Scan() {
+		line := buffer.Text()
+
+		check := [3]string{"red", "blue", "green"}
+
+		result := strings.Split(line, ":")
+
+		id, _ := strconv.Atoi(strings.TrimSpace(strings.SplitAfter(result[0], " ")[1]))
+
+		game := Game{id: id, red: 0, blue: 0, green: 0}
+
+		tries := strings.Split(result[1], ";")
+		for i := 0; i < len(tries); i++ {
+			tries[i] = strings.TrimSpace(tries[i])
+			cube := strings.Split(tries[i], ",")
+			for i := 0; i < len(cube); i++ {
+				cube[i] = strings.TrimSpace(cube[i])
+				roll := strings.Split(cube[i], " ")
+				for i := 0; i < len(check); i++ {
+					if roll[1] == check[i] {
+						if check[i] == "red" {
+							red, _ := strconv.Atoi(roll[0])
+							if game.red < red {
+								game.red = red
+							}
+						} else if check[i] == "blue" {
+							blue, _ := strconv.Atoi(roll[0])
+							if game.blue < blue {
+								game.blue = blue
+							}
+						} else if check[i] == "green" {
+							green, _ := strconv.Atoi(roll[0])
+							if game.green < green {
+								game.green = green
+							}
+						}
+					}
+				}
+			}
+		}
+		// one line change, did the accidental setup in part 1 lol
+		sum += game.red * game.blue * game.green
+	}
+	return sum
 }
 
 func valid_check(game Game) bool {
