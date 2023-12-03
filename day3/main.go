45b67d92bb9a869bf7844640cdffff03e3e84d7d
devonfox <devonhouston.fox@gmail.com>
2023-12-02 22:47:57 -0800

    day3 part1 started

--- day3/main.go
+++ day3/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"os"
+)
+
+func part1() int {
+	puzzleInput := "input_sample.txt"
+
+	file, _ := os.Open(puzzleInput)
+
+	buffer := bufio.NewScanner(file)
+
+	sum := 0
+	inputArray := []string{}
+
+	for buffer.Scan() {
+		line := buffer.Text()
+		inputArray = append(inputArray, line)
+		// println(line)
+	}
+	for i := 0; i < len(inputArray); i++ {
+
+	}
+	return sum
+}
+
 func main() {
-	println("Let's go!")
+	println("Part 1: ", part1())
+
 }
