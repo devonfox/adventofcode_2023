b8829b56596f0cfa44e6937a53d293bc440f9b40
devonfox <devonhouston.fox@gmail.com>
2023-12-03 23:29:18 -0800

    day3 part1 complete

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Solutions in Golang for the [2023 Advent of Code](https://adventofcode.com/2023)
 
 I chose Go as I'd never once used it, and wanted some exposure.
 
-Last Completed: *Day 2, Part 2*
+Last Completed: *Day 3, Part 1*
 
-Star Count: *4*
+Star Count: *5*
--- day3/main.go
+++ day3/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bufio"
 	"os"
+	"strconv"
+	"unicode"
 )
 
 func part1() int {
-	puzzleInput := "input_sample.txt"
+	//4361 test output
+	puzzleInput := "input.txt"
 
 	file, _ := os.Open(puzzleInput)
 
@@ -21,12 +24,91 @@ func part1() int {
 		// println(line)
 	}
 	for i := 0; i < len(inputArray); i++ {
-
+		var look []byte
+		validWord := []bool{}
+		// println(inputArray[i])
+		for j := 0; j < len(inputArray[i]); j++ {
+			if unicode.IsDigit(rune(inputArray[i][j])) {
+				look = append(look, inputArray[i][j])
+				isValid := checkValid(i, j, inputArray)
+				validWord = append(validWord, isValid)
+			} else {
+				if len(look) > 0 && !checkWord(validWord) {
+					// println(string(look))
+					num, _ := strconv.Atoi(string(look))
+					sum += num
+				}
+				look = []byte{}
+				validWord = []bool{}
+			}
+		}
+		if len(look) > 0 && !checkWord(validWord) {
+			// println(string(look))
+			num, _ := strconv.Atoi(string(look))
+			sum += num
+		}
 	}
+
 	return sum
 }
 
+// This is a monstrosity, but it works lol
+func checkValid(i int, j int, inputArray []string) bool {
+
+	if i > 0 {
+		if inputArray[i-1][j] != '.' && !unicode.IsDigit(rune(inputArray[i-1][j])) {
+			return false
+		}
+	}
+	if i < len(inputArray)-1 {
+		if inputArray[i+1][j] != '.' && !unicode.IsDigit(rune(inputArray[i+1][j])) {
+			return false
+		}
+	}
+	if j > 0 {
+		if inputArray[i][j-1] != '.' && !unicode.IsDigit(rune(inputArray[i][j-1])) {
+			return false
+		}
+	}
+	if j < len(inputArray[i])-1 {
+		if inputArray[i][j+1] != '.' && !unicode.IsDigit(rune(inputArray[i][j+1])) {
+			return false
+		}
+	}
+	if i > 0 && j > 0 {
+		if inputArray[i-1][j-1] != '.' && !unicode.IsDigit(rune(inputArray[i-1][j-1])) {
+			return false
+		}
+	}
+	if i > 0 && j < len(inputArray[i])-1 {
+		if inputArray[i-1][j+1] != '.' && !unicode.IsDigit(rune(inputArray[i-1][j+1])) {
+			return false
+		}
+	}
+	if i < len(inputArray)-1 && j > 0 {
+		if inputArray[i+1][j-1] != '.' && !unicode.IsDigit(rune(inputArray[i+1][j-1])) {
+			return false
+		}
+	}
+	if i < len(inputArray)-1 && j < len(inputArray[i])-1 {
+		if inputArray[i+1][j+1] != '.' && !unicode.IsDigit(rune(inputArray[i+1][j+1])) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func checkWord(valid []bool) bool {
+	for i := 0; i < len(valid); i++ {
+		if !valid[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	println("Part 1: ", part1())
+	println("\nPart 1: ", part1())
 
 }
